d37c7bc2ffa5ccf4ee6e90d43e6c492ac056c9ad
Fabio Daros <fabiolaww@gmail.com>
2024-02-14 01:38:09 +0000

    second-part-implementing-marketplace

--- utils/marketplace.py
+++ utils/marketplace.py
@@ -16,18 +16,75 @@ def main() -> None:
 
 
 def menu() -> None:
-    pass
+    print('==================================')
+    print('============ Welcome =============')
+    print('========== Marketplace ===========')
+    print('==================================')
+    print('Select an option: ')
+    print('1. Register products')
+    print('2. List products')
+    print('3. Buy products')
+    print('4. View cart')
+    print('5. Close order')
+    print('6. Exit')
+    print('----------------------------------')
+
+    option: int = int(input('Option: '))
+
+    if option == 1:
+        register_product()
+    elif option == 2:
+        list_products()
+    elif option == 3:
+        buy_product()
+    elif option == 4:
+        view_cart()
+    elif option == 5:
+        close_order()
+    elif option == 6:
+        print('Thank you for your business!')
+        sleep(2)
+        exit(0)
+    else:
+        print('Invalid option')
+        sleep(1)
+        menu()
 
 
 def register_product() -> None:
-    pass
+    print('================')
+    print('Register Product')
+    print('================')
+
+    name: str = input('Product Name: ')
+    price: float = float(input('Product Price: '))
+
+    product: Product = Product(name, price)
+
+    products.append(product)
+
+    print(f'Product {product.name} registered successfully!')
+    sleep(2)
+    menu()
 
 
 def list_products() -> None:
-    pass
+    if len(products) > 0:
+        print('-----------------')
+        print('List of products:')
+        print('-----------------')
+        for product in products:
+            print(product)
+            print('----------------')
+            sleep(1)
+            menu()
+    else:
+        print('No products registered yet!')
+        sleep(2)
+        menu()
 
 
-def add_product() -> None:
+def buy_product() -> None:
     pass
 
 
@@ -36,11 +93,33 @@ def view_cart() -> None:
 
 
 def close_order() -> None:
-    pass
+    if len(cart) > 0:
+        total_value: float = 0
+
+        print('Cart Items: ')
+        for item in cart:
+            for data in item.items():
+                print(data[0])
+                print(f'Quantity: {data[1]}')
+                total_value += data[0].price * data[1]
+                print('---------------------')
+        print(f'Your invoice is {format_float_coin(total_value)}')
+        print('Thank You! Always Time!')
+        cart.clear()
+        sleep(5)
+    else:
+        print('Cart is empty!')
+        sleep(2)
+        menu()
 
 
 def get_product_by_code(code: int) -> Product:
-    pass
+    p: Product = None
+
+    for product in products:
+        if product.code == code:
+            p = product
+    return p
 
 
 if __name__ == '__main__':
